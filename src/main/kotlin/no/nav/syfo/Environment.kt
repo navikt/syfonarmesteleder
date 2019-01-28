16d2f0d31ca5045f43ed053efd4fd61c7dce0a05
Tia Firing <tia.firing@nav.no>
2019-01-28 15:26:49 +0100

    Endrer slik at vi henter ut credentials fra vault via en json-fil

--- src/main/kotlin/no/nav/syfo/Environment.kt
+++ src/main/kotlin/no/nav/syfo/Environment.kt
@@ -1,11 +1,23 @@
 package no.nav.syfo
 
+import com.google.gson.Gson
+import java.io.File
+
 data class Environment(
-    val applicationPort: Int = getEnvVar("APPLICATION_PORT", "8080").toInt(),
-    val applicationThreads: Int = getEnvVar("APPLICATION_THREADS", "4").toInt(),
-    val clientId: String = getEnvVar("clientid"),
-    val clientSecret: String = getEnvVar("clientsecret")
+        val applicationPort: Int = getEnvVar("APPLICATION_PORT", "8080").toInt(),
+        val applicationThreads: Int = getEnvVar("APPLICATION_THREADS", "4").toInt(),
+        val credentials: VaultCredentials = Gson().fromJson(readFileDirectlyAsText(vaultApplicationPropertiesPath), VaultCredentials::class.java)
 )
 
 fun getEnvVar(varName: String, defaultValue: String? = null) =
         System.getenv(varName) ?: defaultValue ?: throw RuntimeException("Missing required variable \"$varName\"")
+
+val vaultApplicationPropertiesPath = "/var/run/secrets/nais.io/vault/secrets.json"
+
+data class VaultCredentials(
+        val clientId: String = getEnvVar("clientid"),
+        val clientSecret: String = getEnvVar("clientsecret")
+)
+
+fun readFileDirectlyAsText(fileName: String): String
+        = File(fileName).readText(Charsets.UTF_8)
