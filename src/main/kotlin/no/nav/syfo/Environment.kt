20329debce94eeb576cfad23974db2fd79b221fa
Tia Firing <tia.firing@nav.no>
2019-01-28 14:33:27 +0100

    Henter clientid og clientsecret fra vault

--- src/main/kotlin/no/nav/syfo/Environment.kt
+++ src/main/kotlin/no/nav/syfo/Environment.kt
@@ -2,7 +2,9 @@ package no.nav.syfo
 
 data class Environment(
     val applicationPort: Int = getEnvVar("APPLICATION_PORT", "8080").toInt(),
-    val applicationThreads: Int = getEnvVar("APPLICATION_THREADS", "4").toInt()
+    val applicationThreads: Int = getEnvVar("APPLICATION_THREADS", "4").toInt(),
+    val clientId: String = getEnvVar("clientid"),
+    val clientSecret: String = getEnvVar("clientsecret")
 )
 
 fun getEnvVar(varName: String, defaultValue: String? = null) =
