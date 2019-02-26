54b834e0830f749f57736f1a7871ee4bbfd91ca1
Tia Firing <tia.firing@nav.no>
2019-02-26 10:41:26 +0100

    Legger til ekstra sjekk av consumerid/callid siden vi ikke vet hvorfor de forsvinner noen ganger

--- src/main/kotlin/no/nav/syfo/narmestelederapi/NarmesteLederClient.kt
+++ src/main/kotlin/no/nav/syfo/narmestelederapi/NarmesteLederClient.kt
@@ -6,6 +6,8 @@ import io.ktor.client.request.get
 import io.ktor.client.request.headers
 import io.ktor.http.ContentType
 import no.nav.syfo.AccessTokenClient
+import no.nav.syfo.missingCallId
+import no.nav.syfo.missingConsumerId
 import org.slf4j.MDC
 import java.time.LocalDate
 
@@ -21,8 +23,8 @@ class NarmesteLederClient(
             accept(ContentType.Application.Json)
             headers {
                 append("Authorization", "Bearer $accessToken")
-                append("Nav-Consumer-Id", MDC.get("Nav-Consumer-Id"))
-                append("Nav-Callid", MDC.get("Nav-Callid"))
+                append("Nav-Consumer-Id", MDC.get("Nav-Consumer-Id") ?: missingConsumerId("hentNarmesteLederFraSyfoserviceStrangler"))
+                append("Nav-Callid", MDC.get("Nav-Callid") ?: missingCallId("hentNarmesteLederFraSyfoserviceStrangler"))
             }
         }.map {
             NarmesteLederRelasjon(
@@ -44,8 +46,8 @@ class NarmesteLederClient(
             accept(ContentType.Application.Json)
             headers {
                 append("Authorization", "Bearer $accessToken")
-                append("Nav-Consumer-Id", MDC.get("Nav-Consumer-Id"))
-                append("Nav-Callid", MDC.get("Nav-Callid"))
+                append("Nav-Consumer-Id", MDC.get("Nav-Consumer-Id") ?: missingConsumerId("hentNarmesteLederForSykmeldtFraSyfoserviceStrangler"))
+                append("Nav-Callid", MDC.get("Nav-Callid") ?: missingCallId("hentNarmesteLederForSykmeldtFraSyfoserviceStrangler"))
             }
         }.let {
             it.narmesteLeder
