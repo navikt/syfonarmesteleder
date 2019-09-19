0639647f6b325c8f0966fb2dc6eeb65391ef40d7
Martin Solheim <martin.solheim@me.com>
2019-09-19 10:35:09 +0200

    Setter opp initielle tabeller i database

---
+++ src/main/resources/db/migration/V0_1__sett_opp_nl_tabell.sql
@@ -0,0 +1,19 @@
+CREATE TABLE NARMESTE_LEDER
+(
+    narmeste_leder_id            VARCHAR(36) PRIMARY KEY,
+    orgnummer                    VARCHAR(64) NOT NULL,
+    bruker_fnr                   VARCHAR(64) NOT NULL,
+    narmeste_leder_fnr           VARCHAR(64) NOT NULL,
+    narmeste_leder_telefonnummer VARCHAR(64) NOT NULL,
+    narmeste_leder_epost         VARCHAR(64) NOT NULL,
+    aktiv_fom                    TIMESTAMP   NOT NULL,
+    aktiv_tom                    TIMESTAMP
+);
+
+CREATE TABLE FORSKUTTERING
+(
+    bruker_fnr                VARCHAR(64) PRIMARY KEY,
+    orgnummer                 VARCHAR(64) PRIMARY KEY,
+    arbeidsgiver_forskutterer BOOLEAN   NOT NULL,
+    sist_oppdatert            TIMESTAMP NOT NULL
+)
