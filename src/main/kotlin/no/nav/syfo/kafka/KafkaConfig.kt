b3f15778c3bd3e41b0a5c8a519c36de04400c6c1
Martin Solheim <martin.solheim@me.com>
2019-09-30 15:18:25 +0200

    Bruker uuid som group id for a spole gjennom alt i testing

--- src/main/kotlin/no/nav/syfo/kafka/KafkaConfig.kt
+++ src/main/kotlin/no/nav/syfo/kafka/KafkaConfig.kt
@@ -7,6 +7,7 @@ import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.common.serialization.StringSerializer
 import java.util.Properties
+import java.util.UUID
 
 fun loadBaseConfig(env: Environment): Properties = Properties().also {
     it.load(KafkaConfig::class.java.getResourceAsStream("/kafka_base.properties"))
@@ -16,11 +17,9 @@ fun loadBaseConfig(env: Environment): Properties = Properties().also {
     it["specific.avro.reader"] = true
 }
 
-const val GROUP_ID = "syfonarmesteleder-consumer"
-
 fun Properties.toConsumerConfig(): Properties = Properties().also {
     it.putAll(this)
-    it[ConsumerConfig.GROUP_ID_CONFIG] = GROUP_ID
+    it[ConsumerConfig.GROUP_ID_CONFIG] = UUID.randomUUID()
     it[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
     it[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
     it[ConsumerConfig.MAX_POLL_RECORDS_CONFIG] = "1000"
@@ -28,7 +27,7 @@ fun Properties.toConsumerConfig(): Properties = Properties().also {
 
 fun Properties.toProducerConfig(): Properties = Properties().also {
     it.putAll(this)
-    it[ConsumerConfig.GROUP_ID_CONFIG] = GROUP_ID
+    it[ConsumerConfig.GROUP_ID_CONFIG] = UUID.randomUUID()
     it[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
     it[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
 }
